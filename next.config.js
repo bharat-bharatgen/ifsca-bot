784fb023573cbd7e0cc1c47e03d9a532000cf116
Bharat Patil <bharat.patil@tihiitb.org>
2026-01-17 13:16:14 +0530

    create prod docker image and docker compose to run it

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '2mb',
-    },
-  },
+  // experimental: {
+  //   serverActions: {
+  //     bodySizeLimit: '2mb',
+  //   },
+  // },
+  output: "standalone",
+
 };
 
 module.exports = nextConfig;
